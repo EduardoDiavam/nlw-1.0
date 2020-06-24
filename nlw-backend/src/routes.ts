9265f3151f1f79e50cbd0c6a0660e9a96d2af556
EduardoDiavam <macedoeduardo99@gmail.com>
2020-06-24 20:27:12 -0300

    App

---
+++ nlw-backend/src/routes.ts
@@ -0,0 +1,19 @@
+import express from 'express';
+
+import PointsController from './controllers/PointsController'
+const pointsController = new PointsController();
+
+import ItemsController from './controllers/ItemsController';
+const itemsController = new ItemsController();
+
+const routes = express.Router();
+
+routes.get('/items', itemsController.index);
+
+routes.post('/points', pointsController.create);
+routes.get('/points/:id', pointsController.show);
+routes.get('/points', pointsController.index);
+
+
+
+export default routes;
